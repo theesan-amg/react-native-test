d06c3a4e07ea18f866db0c8e1584c13ad5d9a325
theesan-amg <theesan270@outlook.com>
2021-09-21 14:26:35 +0200

    Test exercice 1 finished

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image , Pressable, Alert, ActivityIndicator} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.text}>
+        <Text style={styles.text1}>open </Text>
+        <Text style={styles.text2}>hello</Text>
+        <Text style={styles.text3}>World</Text>
+      </View>
+      <Image
+        source={require("../test/assets/logo.png")}
+        style={{ width: 200, height: 65 }}>
+      </Image>
+      <Pressable style={styles.press} onPress={() => Alert.alert("Vous avez appuyer")}><Text>Press me</Text></Pressable>
+  
+      <ActivityIndicator size="large" color="white"></ActivityIndicator>
+    </ScrollView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  container:{
+   
+    backgroundColor: "red"
   },
+
+  text: {
+    backgroundColor: "white",
+    marginTop: "80px",
+    marginBottom: "80px",
+  },
+  text1: {
+    fontSize: 30,
+  },
+  text2: {
+    textAlign: "center",
+  },
+  text3: {
+    fontWeight: "bold",
+  },
+  press:{
+    fontWeight: "bold",
+    textAlign:"center"
+  }
 });
